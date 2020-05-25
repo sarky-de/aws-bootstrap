8b84b0c4852f425d89ae000a446471de1c5db3f8
Jay Allan Jethwa <git@jethwa.de>
2020-05-24 17:22:40 -0700

    Hello, Cloud!

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const { hostname } = require('os');
-const http = require('http');
-const message = 'Hello World\n'; 
-const port = 8080; 
-const server = http.createServer((req, res) => { 
+const { hostname } = require("os");
+const http = require("http");
+const message = "Hello Cloud\n";
+const port = 8080;
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader("Content-Type", "text/plain");
   res.end(message);
 });
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname()}:${port}/`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname()}:${port}/`);
 });
